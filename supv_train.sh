663f20dd59b6c5b8eed1bb37ee2b7b5b68a58f10
TimeInDreams <1543165754@qq.com>
2023-04-16 18:35:34 +0800

    调节参数batch_size=128，修改checkpoint path

--- supv_train.sh
+++ supv_train.sh
@@ -2,8 +2,8 @@ python supv_main.py \
 --gpu 0 \
 --lr 0.0007 \
 --clip_gradient 0.5 \
---snapshot_pref "./Exps/Supv/exp1/" \
+--snapshot_pref "../drive/MyDrive/Exps/Supv/exp1/" \
 --n_epoch 200 \
---b 64 \
+--b 128 \
 --test_batch_size 16 \
 --print_freq 1

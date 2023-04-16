6111ed56aa4bd79d3afc68c0d56d8d470d252fc0
TimeInDreams <1543165754@qq.com>
2023-04-16 18:41:24 +0800

    train中添加resume checkpoint

--- supv_train.sh
+++ supv_train.sh
@@ -3,6 +3,7 @@ python supv_main.py \
 --lr 0.0007 \
 --clip_gradient 0.5 \
 --snapshot_pref "../drive/MyDrive/Exps/Supv/exp1/" \
+--resume "../drive/MyDrive/Exps/Supv/exp1/model_epoch_11_top1_75.672_task_Supervised_best_model.pth.tar"
 --n_epoch 200 \
 --b 128 \
 --test_batch_size 16 \
